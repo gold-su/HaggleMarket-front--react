c8d723a5b74ad271d84674f12f620b6646cbf59b
박동수 <pds020130@gmail.com>
2025-11-04 01:00:22 +0900

    서버 수정

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      "/api": "http://localhost:8080", // 백엔드 주소에 맞게 수정
+      "/api": "https://hagglemarket.onrender.com", // 백엔드 주소에 맞게 수정
     },
   },
   define: {
